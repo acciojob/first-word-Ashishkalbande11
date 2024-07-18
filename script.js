1275bf942f0a8acd5032759e66107fbb84fabe81
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-18 19:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function firstWord(s) {
   // your code here
 	if(s === '') return s;
+	s =
+		s.trim();
+	
 
-	let ind = s.indexOf(' ');
+	const ind = s.indexOf(' ');
 	if(ind === -1) {
 		return s;
 	}
 
 	return s.slice(0, ind);
-
 	
 }
 
