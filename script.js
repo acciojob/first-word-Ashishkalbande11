9812f1f6d032234402e6dbbe1c459538ca7b5402
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-18 15:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	let ind;
+    for (let ch in s) {
+        if (s[ch] === " ") {
+            ind = ch;
+            break;
+        }
+    }
+    return s.substring(0, ind);
 }
 
 // Do not change the code below
